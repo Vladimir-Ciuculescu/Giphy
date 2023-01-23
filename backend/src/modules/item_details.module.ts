946e266bc968086efc12b77b5f318e164ea482fc
BorislavVasiliev <borislav.vasiliev@vspartners.us>
2023-01-23 15:05:58 +0200

    item details endpoints added

--- backend/src/modules/item_details.module.ts
+++ backend/src/modules/item_details.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItemDetailsController } from 'src/controllers/item_details.controller';
 import { Item_Details } from 'src/entities';
+import { ItemDetailsService } from 'src/services/item_details.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Item_Details])],
-  controllers: [],
-  providers: [],
+  controllers: [ItemDetailsController],
+  providers: [ItemDetailsService],
 })
 export class ItemDetailsModule {}
